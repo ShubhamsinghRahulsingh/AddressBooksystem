3d9d3ed215d18b323be60a8530c94d122b0b45a0
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-07 13:10:57 +0530

    (Shubham)Add: Creating contact details in Address Book

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,6 +6,19 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            Contact contact = new Contact() //object creation
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToInt64(Console.ReadLine()),
+                PhoneNUmber = Console.ReadLine(),
+                Email = Console.ReadLine()
+
+            };
+            Console.WriteLine("Contact Details:"+"\n"+"FirstName: "+contact.FirstName+"\n"+"LastName: "+contact.LastName+"\n"+"Address: "+contact.Address+"\n"+"City: "+contact.City+"\n"+"State: "+contact.State+"\n"+"Zip: "+contact.Zip+"\n"+"PhoneNumber: "+contact.PhoneNUmber+"\n"+"Email: "+contact.Email);
 
         }
     }
