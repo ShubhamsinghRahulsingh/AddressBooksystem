12679ccd6b0369d343dfeb964a8a6e5270e16be9
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-11 16:06:40 +0530

    (Shubham)Refactor: Ensure No Duplicate data entry in Particular AddressBook

--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Xml.Linq;
 
 namespace AddressBook
@@ -10,24 +11,24 @@ namespace AddressBook
         Dictionary<string, List<Contact>> addressBook = new Dictionary<string, List<Contact>>();  
         public void CreateContact()
         {
-            Contact contact = new Contact();
-            Console.WriteLine("Enter the firstName");
-            contact.FirstName = Console.ReadLine();
-            Console.WriteLine("Enter the lastName");
-            contact.LastName = Console.ReadLine();
-            Console.WriteLine("Enter the Address");
-            contact.Address = Console.ReadLine();
-            Console.WriteLine("Enter the City");
-            contact.City = Console.ReadLine();
-            Console.WriteLine("Enter the State");
-            contact.State = Console.ReadLine();
-            Console.WriteLine("Enter the Zip");
-            contact.Zip = Convert.ToInt64(Console.ReadLine());
-            Console.WriteLine("Enter the PhoneNumber");
-            contact.PhoneNUmber = Console.ReadLine();
-            Console.WriteLine("Enter the Email ID");
-            contact.Email = Console.ReadLine();
-            address.Add(contact);
+                Contact contact = new Contact();
+                Console.WriteLine("Enter the firstName");
+                contact.FirstName = Console.ReadLine();
+                Console.WriteLine("Enter the lastName");
+                contact.LastName = Console.ReadLine();
+                Console.WriteLine("Enter the Address");
+                contact.Address = Console.ReadLine();
+                Console.WriteLine("Enter the City");
+                contact.City = Console.ReadLine();
+                Console.WriteLine("Enter the State");
+                contact.State = Console.ReadLine();
+                Console.WriteLine("Enter the Zip");
+                contact.Zip = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine("Enter the PhoneNumber");
+                contact.PhoneNUmber = Console.ReadLine();
+                Console.WriteLine("Enter the Email ID");
+                contact.Email = Console.ReadLine();
+                CheckForDuplicacy(address, contact);              
         }
         public void EditContact()
         {
@@ -88,6 +89,18 @@ namespace AddressBook
                 Console.WriteLine("Contact Details:" + "\n" + "FirstName: " + contact.FirstName + "\n" + "LastName: " + contact.LastName + "\n" + "Address: " + contact.Address + "\n" + "City: " + contact.City + "\n" + "State: " + contact.State + "\n" + "Zip: " + contact.Zip + "\n" + "PhoneNumber: " + contact.PhoneNUmber + "\n" + "Email: " + contact.Email);
             }
 
+        }
+        public void CheckForDuplicacy(List<Contact> address,Contact contact)
+        {
+            if (address.Any())
+            { 
+                    if (address.Any(e=>e.FirstName == contact.FirstName))//Lambda expression
+                    {
+                        Console.WriteLine("A person with name {0} is already existed", contact.FirstName);
+                        return;
+                    }
+            }
+                address.Add(contact);
         }
         public void CreateDictionary()
         {
